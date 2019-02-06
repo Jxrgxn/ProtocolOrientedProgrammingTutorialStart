82073858b4a8090c4d9c03a04db30c34bb210de3
Jxrgxn <basel.farag@gmail.com>
2019-02-06 00:31:01 -0700

    First Commit

---
+++ ProtocolOrientedProgrammingTutorialStart/Model/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  ProtocolOrientedProgrammingTutorialStart
+//
+//  Created by Basel on 2/1/19.
+//  Copyright © 2019 BaselFarag. All rights reserved.
+//
+
+import Foundation
